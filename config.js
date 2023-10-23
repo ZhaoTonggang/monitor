3e1124f79aaa2fa33bb9a0e0870e7d8d3a4dde22
root <root@cvm-3iqxkwe2da225.novalocal>
2023-10-23 16:56:54 +0800

    update

---
+++ config.js
@@ -0,0 +1,28 @@
+window.Config = {
+
+	// 显示标题
+	SiteName: '服务器感知',
+
+	// UptimeRobot Api Keys
+	// 支持 Monitor-Specific 和 Read-Only
+	ApiKeys: [
+		'ur1230502-075ac55a37c7d718f2f153a1',
+	],
+
+	// 日志天数
+	CountDays: 90,
+
+	// 是否显示检测站点的链接
+	ShowLink: true,
+
+	// 导航栏菜单
+	Navi: [{
+			text: '博客',
+			url: 'https://blog.heheda.top/'
+		},
+		{
+			text: '主页',
+			url: 'https://my.heheda.top/'
+		},
+	],
+};
